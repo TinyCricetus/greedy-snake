d13b72954b212851527a8157baa13494ae338694
lizexi <lizexi@cmcm.com>
2022-03-23 19:38:58 +0800

    feat: 全面支持typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,28 @@
-const path = require('path')
-const htmlWebpackPlugin = require('html-webpack-plugin')
-
+"use strict";
+exports.__esModule = true;
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-  mode: 'production',
-
-  entry: path.resolve(__dirname, 'index.ts'),
-
-  // devtool: "inline-source-map",
-
-  output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, 'dist'),
-    clean: true
-  },
-
-  plugins: [new htmlWebpackPlugin({
-    template: 'index.html'
-  })],
-
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx']
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        loader: "ts-loader"
-      }
-    ]
-  }
-}
+    mode: 'production',
+    entry: path.resolve(__dirname, 'index.ts'),
+    // devtool: "inline-source-map",
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist'),
+        clean: true
+    },
+    plugins: [new HtmlWebpackPlugin({
+            template: 'index.html'
+        })],
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: "ts-loader"
+            }
+        ]
+    }
+};
---
+++ webpack.config.ts
@@ -0,0 +1,33 @@
+import path = require('path')
+import HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'production',
+
+  entry: path.resolve(__dirname, 'index.ts'),
+
+  // devtool: "inline-source-map",
+
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist'),
+    clean: true
+  },
+
+  plugins: [new HtmlWebpackPlugin({
+    template: 'index.html'
+  })],
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: "ts-loader"
+      }
+    ]
+  }
+}
